c006956714941b55b952c02687416a8cdea0d7d6
repcsi90 <stumph.robert.andras@diak.szamalk-szalezi.hu>
2024-10-24 22:08:09 +0200

    rekordok

--- database/migrations/2024_10_24_185705_create_books_table.php
+++ database/migrations/2024_10_24_185705_create_books_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Book;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,6 +20,12 @@ return new class extends Migration
             $table->integer('pieces')->default(100);
             
         });
+
+        Book::create([
+            'author' => 'Nádas Péter', 
+            'title' => 'Rémtörténetek',
+            'pieces'=> 200 
+            ]);
     }
 
     /**
--- database/migrations/2024_10_24_186252_create_copies_table.php
+++ database/migrations/2024_10_24_186252_create_copies_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Copy;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,10 @@ return new class extends Migration
             $table->foreignId('user_id')->references('id')->on('users');
             $table->timestamps();
         });
+        Copy::create([
+            'book_id' => 10, 
+            'user_id' => 1, 
+            ]);
     }
 
     /**
